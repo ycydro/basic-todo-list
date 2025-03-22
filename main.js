ea4883536c80e38c2cc28941860d4a2a05ec6cc9
ycydro <ycydro@users.noreply.github.com>
2025-03-22 22:42:52 +0800

    my own version of supersimpledev's todo-list from his JS course

---
+++ main.js
@@ -0,0 +1,96 @@
+let taskArr = [];
+function addTask() {
+   const taskName = document.getElementById('task-input');
+   const taskDeadline = document.getElementById('deadline');
+   // increment the taskId depending on length of the task array
+   const taskId = taskArr.length + 1;
+
+   // checks if input is invalid
+   if (!taskName.value || !taskDeadline.value) {
+      displayStatusMessage('error', 'Please insert valid inputs!')
+      return;
+   }
+
+   // adds a task object with id, name, and deadline property
+   taskArr.push({id: taskId, name: taskName.value, deadline: taskDeadline.value});
+
+   // add the task row to the dom
+
+   displayTask(taskId, taskName.value, taskDeadline.value);
+   displayStatusMessage('success', 'Successfuly added task!')
+   clearInputs();
+}
+
+
+function deleteTask(taskId) {
+   // retrieves the div row with a class name of taskId 
+   // i.e. taskId = task1
+   const taskRow = document.querySelector(`.${taskId}`);
+
+    for (let i = 0; i < taskArr.length; i++) {
+      // retrieve the integer from taskId
+      // i.e. if taskId is 'task1', remove 'task' to get '1'
+      const realTaskId = taskId.replace('task', '');
+
+      // turn string to number before comparing
+      if (Number(realTaskId) === taskArr[i].id) {
+         // remove element in index 'i'
+         taskArr.splice(i, 1)
+         // remove the row corresponding to taskId
+         taskRow.remove();
+         displayStatusMessage('error', 'Deleted a task!')
+         break;
+      }
+   }
+   console.log(taskArr.length);
+}
+
+function displayTask(id, name, deadline) {
+   // retrives the container where the task rows will be placed
+   const taskContainer = document.querySelector('.tasks');
+   // add a task row
+   taskContainer.innerHTML += `
+   <div class="todo-row task${id}">
+   <div class="todo-name">${name}</div>
+   <div class="todo-date">${deadline}</div>
+   <button id="task${id}" 
+      class="task-btn delete-button" 
+      onclick="deleteTask(this.id)"
+   >
+      Delete
+   </button>`
+}
+
+function displayStatusMessage(status, message) {
+   const statusElement = document.querySelector('.status-message');
+
+   removeCurrentStatus();
+
+   if (status === 'error') {
+      statusElement.classList.add('error-message');
+   } else if (status === 'success')  {
+      statusElement.classList.add('success-message');
+   }
+
+   statusElement.innerHTML = message;
+}
+
+function removeCurrentStatus() {
+   const statusElement = document.querySelector('.status-message');
+   if(statusElement.classList.contains('error-message')) {
+      statusElement.classList.remove('error-message')
+   } else if (statusElement.classList.contains('success-message')) {
+      statusElement.classList.remove('success-message')
+   } else {
+      return;
+   }
+}
+
+function clearInputs() {
+   const taskName = document.getElementById('task-input');
+   const taskDeadline = document.getElementById('deadline');
+
+   taskName.value = '';
+   taskDeadline.value = null;
+}
+
